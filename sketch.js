0744ddf1f29c527f14b360790a70fbc50cc83d04
lmsol1030 <lmsol1030@naver.com>
2014-10-16 15:42:29 +0900

    s

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
-function set up()
+function setup()
 {
-	creatCanvers(800,800)
+	createCanvas(800,800)
 	background(0);
 }
 
 function draw()
 {
+	background(0);
 	fill(255);
 	ellipse(mouseX, mouseY, 100, 100);
 }
